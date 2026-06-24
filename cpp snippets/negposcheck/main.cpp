36e2d54bf72289e3cf909334cd769d711ed61a8b
saimdlx <1629saim@gmail.com>
2026-06-24 12:52:34 -0700

    (add) cpp snippets portion

---
+++ cpp snippets/negposcheck/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+    //exhaust user inputs until zero is pressed, return primary numerical counts
+    int pos = 0;
+    int neg = 0;
+    int input = 0;
+
+    cin >> input;
+    while (input){
+        if (input > 0){
+            pos++;
+        }
+        if (input < 0){
+            neg++;
+        }
+        cin >> input;
+    }
+    if (pos > neg){
+        cout << "More positives" << endl;
+    }
+    else if (neg > pos){
+        cout << "More negatives" << endl;
+    }
+    else {
+        cout << "They're equal" << endl;
+    }
+    
+    return 0;
+
+}
